985b347a84ace0bce5afa17cdc21c2b3138db154
Isaque <iskschluter@gmail.com>
2021-08-23 16:23:15 -0300

    add everything to validate.ts

--- validate.ts
+++ validate.ts
@@ -1,11 +1,26 @@
-import CepValidation from "./src/genericValidations/cepValidation";
-import CpfValidation from "./src/genericValidations/cpfValidation";
-import PhoneValidation from "./src/genericValidations/phoneValidation";
+// Base classes
 import InputValidation from "./src/inputValidation";
 import MultipleValidation from "./src/multipleValidation";
 import RegexValidation from "./src/regexValidation";
 
+// Validations
+import CepValidation from "./src/genericValidations/cepValidation";
+import CpfValidation from "./src/genericValidations/cpfValidation";
+import PhoneValidation from "./src/genericValidations/phoneValidation";
+import DateValidation from "./src/genericValidations/dateValidation";
+import RenavamValidation from "./src/genericValidations/renavamValidation";
+import VehicleMercosulPlateValidation from "./src/genericValidations/vehicleMercosulPlateValidation";
+import VehicleOldPlateValidation from "./src/genericValidations/vehicleOldPlateValidation";
+import VehiclePlateValidation from "./src/genericValidations/vehiclePlateValidation";
+import VinValidation from "./src/genericValidations/vinValidation";
+
 export {
+    DateValidation,
+    RenavamValidation,
+    VehicleMercosulPlateValidation,
+    VehicleOldPlateValidation,
+    VehiclePlateValidation,
+    VinValidation,
     CepValidation,
     CpfValidation,
     PhoneValidation,
